5ebba3446f25fcc2d270f318806e0be537ee623f
Evrard Van Espen <evrard.vanespen@arke.fr>
2025-11-06 20:36:59 +0000

    feat: add resizer service main file

---
+++ backend_microservices/resizer_service/src/main.rs
@@ -0,0 +1,31 @@
+use bytes::Bytes;
+use futures_util::StreamExt;
+
+#[derive(Debug)]
+struct NatsMessage {
+    key: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), async_nats::Error> {
+    // Connect to the NATS server
+    let client = async_nats::connect("localhost:4222").await?;
+    println!("NATS connected");
+
+    // Subscribe to the "messages" subject
+    let mut subscriber = client.subscribe("pictures").await?;
+    println!("Subscribed");
+
+    // Receive and process messages
+    while let Some(message) = subscriber.next().await {
+
+	let toto: NatsMessage = rmp_serde::from_slice(&message.payload).unwrap();
+	println!("{:?}", toto);
+	
+        println!("Received message {:?}", message.payload);
+    }
+
+    // test AI!
+
+    Ok(())
+}
